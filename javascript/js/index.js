394e2e14c588aa464529abef5a77052a84a78f2b
emarkovic <em42@uw.edu>
2015-10-13 17:14:44 -0700

    dom js lecture

--- javascript/js/index.js
+++ javascript/js/index.js
@@ -1,3 +1,28 @@
-/**
- * application script for index.html
- */
+document.addEventListener('DOMContentLoaded', function () {
+    "use strict";
+
+    function forEachElement(collection, fcn) {
+        for (var i = 0; i < collection.length; i++) {
+            fcn(collection[i]);
+        }
+    }
+
+    var clickMeButton = document.getElementById("click-me");
+    clickMeButton.addEventListener("click", function () {
+       var alerts = document.querySelectorAll('.alert');
+        forEachElement(alerts, function (alert) {
+            alert.style.display = 'block';
+        })
+    });
+
+    var closeButtons = document.querySelectorAll(".alert .close");
+
+
+    forEachElement(closeButtons, function (button) {
+
+        button.addEventListener("click", function () {
+
+            button.parentElement.style.display = "none";
+        });
+    })
+});

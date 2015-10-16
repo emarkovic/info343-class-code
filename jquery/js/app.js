0c7779f0b9caa782bec290c17322b8cf2fdae325
iGuest <em42@uw.edu>
2015-10-15 17:12:18 -0700

    map lecture

--- jquery/js/app.js
+++ jquery/js/app.js
@@ -6,3 +6,23 @@
     http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f
 */
 
+$(function () {     //this is the same as document.addEventList...
+    "use strict";
+    var url = 'http://api.openweathermap.org/data/2.5/weather?zip=98195,us&units=imperial&appid=bd82977b86bf27fb59a04b61b657fb6f';
+
+
+    $('a').attr('target', '_blank');
+    $('article').hide().fadeIn();
+
+    $('#toggle-article').on('click', function () {
+        $('article').fadeToggle();
+    });
+
+    $.getJSON(url)
+        .then(function (data) {
+            console.log(data);
+            var temp = data.main.temp;
+            $('#temp').text(Math.round(temp));
+        }
+    );
+})
--- leaflet/js/app.js
+++ leaflet/js/app.js
@@ -8,3 +8,26 @@
     attribution:
     '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 */
+
+
+$(function () {
+    "use strict";
+
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (pos) {
+            createMap([pos.coords.latitude, pos.coords.longitude], 15);
+        }, function (err) {
+            createMap([47.6097, -122.3331], 14);   
+        });
+    } else{
+        createMap([47.6097, -122.3331], 14);    
+    }
+
+    function createMap(loc, zoom) {
+        var map = L.map('map').setView(loc, zoom);
+        L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+        L.circleMarker(loc).addTo(map).bindPopup("hello");
+    }
+})

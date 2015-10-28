182614e72861572e257c9318336a9d9e37d6a7e1
em42 <em42@uw.edu>
2015-10-27 17:16:52 -0700

    10/27 angular lecture #1

--- angular/js/tasks.js
+++ angular/js/tasks.js
@@ -1,4 +1,32 @@
 /* 
     script for the tasks.html file 
 */
+angular.module('Tasks', [])
+    .constant('tasksKey', 'tasks')  //set your constants
+    .controller('TasksController', function($scope, tasksKey) {
+        //dependency injection - person calling fcn should just pass in what they need and the
+        //designer should initialize it
+        
+        //add some variables on scope so that our html has access to that information
 
+        //overall array for every task
+        $scope.tasks = angular.fromJson(localStorage.getItem(tasksKey)) || [];
+        $scope.newTask = {};
+
+        function saveTasks() {
+            localStorage.setItem(tasksKey, angular.toJson($scope.tasks));
+        }
+
+        $scope.addTask = function () {
+            $scope.tasks.push($scope.newTask);
+
+            saveTasks();
+
+            $scope.newTask = {};
+        };
+
+        $scope.toggleDone = function (task) {
+            task.done = !task.done;
+            saveTasks();
+        }
+    });

fbcdff9a0a4a0ed313f24f64fb7c231f5972a543
emarkovic <em42@uw.edu>
2015-10-29 16:34:46 -0700

    movies.html filters for the movies

--- angular/js/movies.js
+++ angular/js/movies.js
@@ -1,7 +1,33 @@
 angular.module('Movies', [])                    //need http for ajax requests in angular
     .controller('MoviesController', function ($scope, $http) {
+        
         $http.get('/data/movies-2014.min.json')
             .then(function (results) {
-                $scope.movies = results.data;
+                var ratingsMap = {
+                    "Not Rated" : 0,
+                    "G" : 1,
+                    "PG" : 2,
+                    "PG-13" : 3,
+                    "R" : 4,
+                    "NC-17" : 5,
+                    "X" : 6
+                }
+
+                $scope.movies = results.data.map(function(movie) {
+                    movie.ratingOrdinal = ratingsMap[movie.rating];
+                    return movie;
+                });
+
+                $scope.distributors = _.uniq(_.pluck($scope.movies, 'distributor'));
             });
+
+        $scope.setSort = function(propertyName) {
+            if ($scope.sortCol === propertyName) {
+                $scope.sortReverse = !$scope.sortReverse;
+            } else {
+                $scope.sortCol = propertyName;    
+                $scope.sortReverse = false;
+            }
+            
+        }
     });
